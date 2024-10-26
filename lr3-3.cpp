20da7872f2ee344236f4815f093fb6cab6bd7852
liana <lianabub07@gmail.com>
2024-10-26 09:10:33 +0300

    Add files via upload

---
+++ lr3-3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <math.h> 
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "RU");
+    int a;
+    cout << "Введите количество коров";
+    cin >> a;
+    if (a % 10 = 0) {
+        cout << "На лугу посется одна корова";
+    }
+    else if (a % 10 > 1 and a % 10 < 5) {
+        cout << "На лугу посется" << a << "коровы";
+    }
+    else {
+        cout << "На лугу посется " << a << "коров";
+    }
+    return 0;
+   
+    
+
