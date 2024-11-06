814b71d5551509a43209f976af74727bbe1e88f6
liana <lianabub07@gmail.com>
2024-11-06 18:16:22 +0300

    Update lr3-3.cpp

--- lr3-3.cpp
+++ lr3-3.cpp
@@ -1,21 +1,31 @@
 ﻿#include <iostream>
-#include <math.h> 
+#include <math.h>
 using namespace std;
-int main() {
-    setlocale(LC_ALL, "RU");
-    int a;
-    cout << "Введите количество коров";
-    cin >> a;
-    if (a % 10 = 0) {
-        cout << "На лугу посется одна корова";
-    }
-    else if (a % 10 > 1 and a % 10 < 5) {
-        cout << "На лугу посется" << a << "коровы";
-    }
-    else {
-        cout << "На лугу посется " << a << "коров";
-    }
-    return 0;
-   
-    
+int main()
+{ 
+	setlocale(LC_ALL, "ru");
+	int n;
+	cout << "введите количество коров";
+	cin >> n;
+	if (n < 100)
+	{
+		if (n != 11 and n % 10 == 1)
+		{
+			cout << "на лугу пасется" << n << "корова";
+		}
+		else if ((n % 10 == 2 and n != 12) || (n % 10 == 3 and n != 13) || (n % 10 == 4 and n != 14))
+		{
+			cout << "на лугу пасется" << n << "коровы";
+		}
+		else
+		{
+			cout << "на лугу пасется" << n << "коров";
+		}
+	}
+	else
+	{
+		cout << "слишком много коров";
+
+	}
+}
 
